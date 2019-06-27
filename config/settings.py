04351bca106822bc3c247a2d57ad1905b9e76047
Nichita Gruia <nichita.gruia@ebs-integrator.com>
2019-06-27 19:07:46 +0300

    Add swagger.

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'apps.common',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 
+from apps.common.helpers import schema_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
