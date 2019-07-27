77e452d1cc92b9d47c7f11e9392011f716f94788
Nichita Gruia <nichita.gruia@ebs-integrator.com>
2019-07-27 17:47:22 +0300

    Add books application.

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from apps.common.helpers import schema_view
+from apps.books import urls as book_urls
 
 urlpatterns = [
     path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("books/", include(book_urls), name='books'),
 ]
